f492e23233bf4f7ebf5ae416c07ade4e1b2486c4
Vinicios Trindade Costa <135274652+Vinhicious@users.noreply.github.com>
2025-06-03 11:01:45 -0300

    Update server.js

--- Aula-05-27-25/server.js
+++ Aula-05-27-25/server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-//rotas
-app.get('/', (req, res) =>{
-    res.send("Front Funcionando")
-});
-
 //criar conexão com o banco
 const db = require('./db');
 
+//disponibilizar o caminho dos arquivos estaticos
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')))
+
+//rotas
+app.get('/', (req, res) =>{
+    //res.send("Front Funcionando")
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use(express.json());//lidar com os dados relacionados ao json
 const apiRoutes = require('./routes/api')
 app.use('/api/users', apiRoutes);
 
